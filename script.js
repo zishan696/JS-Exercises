8ada9f5f2194f25901c08929f32febdba1c53c71
Ahshan Zishan <dantex696@outlook.com>
2018-05-22 18:09:21 +0000

    Task 4

--- script.js
+++ script.js
@@ -22,3 +22,30 @@ function checkEmail(emailString){
 }
 console.log(checkEmail("john@smith.com"));
 console.log(checkEmail("johnsmith.com"));
+
+//Task 4
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+function getReadingStatus(index)
+{
+     return [library[index].author,library[index].title,library[index].readingStatus];
+      return library[index].title;
+    return library[index].readingStatus;
+}
+console.log(getReadingStatus(0));
+console.log(getReadingStatus(1));
+console.log(getReadingStatus(2));
