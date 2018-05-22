ab30e7945566132fdf1d228029ba61993f871cc1
Ahshan Zishan <dantex696@outlook.com>
2018-05-22 17:43:41 +0000

    Task 2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 /* JavaScript code here */
 //Task 1
 console.log("External JS");
+
+//Task 2
+function sumArray(arr)
+{
+    var sum = 0;
+    arr.forEach (function(index, value){
+        sum+= value;
+    });
+    return sum;
+}
+console.log(sumArray([1,2,3,4,5]));
+console.log(sumArray([1,20,-3,45]));
