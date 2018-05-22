70d46266d19e2bfbd4e683c94e7eac707965457c
Ahshan Zishan <dantex696@outlook.com>
2018-05-22 17:51:11 +0000

    Task 3

--- script.js
+++ script.js
@@ -6,10 +6,19 @@ console.log("External JS");
 function sumArray(arr)
 {
     var sum = 0;
-    arr.forEach (function(index, value){
+    arr.forEach (function(value, index){
         sum+= value;
     });
     return sum;
 }
 console.log(sumArray([1,2,3,4,5]));
 console.log(sumArray([1,20,-3,45]));
+
+//Task 3
+function checkEmail(emailString){
+    var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    var match = emailFormat.test(emailString);
+    return match;
+}
+console.log(checkEmail("john@smith.com"));
+console.log(checkEmail("johnsmith.com"));
